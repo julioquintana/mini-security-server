37ce7bdfad5de2da00fd49f936d80dc424614287
Julio Quintana <julio.quintanasilva@cencosud.cl>
2022-06-08 22:37:39 -0400

    create format logging objects

--- src/main/java/cl/minisecurityserver/securityservertest/util/LogFormatter.java
+++ src/main/java/cl/minisecurityserver/securityservertest/util/LogFormatter.java
@@ -1,2 +1,36 @@
-package cl.minisecurityserver.securityservertest.util;public class LogFormatter {
+package cl.minisecurityserver.securityservertest.util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.SneakyThrows;
+import org.slf4j.MDC;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class LogFormatter extends ObjectMapperConfig {
+  private String internalId = MDC.get("internalId");
+  private String method;
+  private String message;
+  private String reason;
+  private Object payload;
+
+  @SneakyThrows
+  @Override
+  public String toString() {
+    String json = "";
+    try {
+      json = objectMapper.writeValueAsString(this);
+
+    } catch (JsonProcessingException e) {
+      e.printStackTrace();
+    }
+    return removeFirstAndLastChar(json);
+  }
+
+  private String removeFirstAndLastChar(String str) {
+    return str.trim().substring(1, str.length() - 1);
+  }
 }

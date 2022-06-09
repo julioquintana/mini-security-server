a06d945b13a741a3bc9ce7c1dc480bcd2cb19244
Julio Quintana <julio.quintanasilva@cencosud.cl>
2022-06-08 21:17:51 -0400

    commit initial functional

---
+++ src/main/java/cl/minisecurityserver/securityservertest/SecurityServerTestApplication.java
@@ -0,0 +1,13 @@
+package cl.minisecurityserver.securityservertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityServerTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityServerTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/minisecurityserver/securityservertest/SecurityServerTestApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.minisecurityserver.securityservertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityServerTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
